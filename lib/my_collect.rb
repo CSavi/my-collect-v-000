d078bc4b984bc9a6aab66676703041b4cbd5d707
Carrie Savinell <carrielane89@gmail.com>
2018-05-05 15:40:00 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+require 'pry'
 
+def my_collect(collection_array)
+  i = 0 
+  collect = []
+  while i < collection_array.length 
+    collect << yield (collection_array[i])
+    i = i + 1
+  end   
+  
+end   
 
