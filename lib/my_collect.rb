b660b137c79c01887ad6a595e7f10cc1086b9323
Carrie Savinell <carrielane89@gmail.com>
2018-05-05 16:13:14 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,19 +1,13 @@
 require 'pry'
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
-def my collect(array) do |name| name.split(" ").first 
-end 
-  
-
-#def my_collect(collection_array)
-#  i = 0 
-#  collect = []
-#  while i < collection_array.length 
-#    collect << yield(collection_array[i])
-#    i = i + 1
-#  end   
-#  collection
-#end   
+def my_collect(collection_array)
+  i = 0 
+  collect = []
+  while i < collection_array.length 
+    collect << yield(collection_array[i])
+    i = i + 1
+  end   
+  collection
+end   
 
 #my_collect(collection_array)
